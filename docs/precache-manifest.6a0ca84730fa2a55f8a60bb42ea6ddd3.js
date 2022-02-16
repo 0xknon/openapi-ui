2f3d134e396bf4722cf3439360040f333846f6f8
knyumatt <knyuwork@gmail.com>
2022-02-16 20:01:49 +0800

    build: github page

---
+++ docs/precache-manifest.6a0ca84730fa2a55f8a60bb42ea6ddd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a435c942a064bb3aaf01602cf4731d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfd37b27c787a93896e",
+    "url": "/static/css/2.f1565476.chunk.css"
+  },
+  {
+    "revision": "2288b5ce4820fd5f4193",
+    "url": "/static/css/main.640b958f.chunk.css"
+  },
+  {
+    "revision": "bcfd37b27c787a93896e",
+    "url": "/static/js/2.815a9338.chunk.js"
+  },
+  {
+    "revision": "bd11e614707c285659803797bece2140",
+    "url": "/static/js/2.815a9338.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2288b5ce4820fd5f4193",
+    "url": "/static/js/main.96b8a603.chunk.js"
+  },
+  {
+    "revision": "afa403bf35c1c6160521",
+    "url": "/static/js/runtime-main.2fcd05ba.js"
+  },
+  {
+    "revision": "dd28d3d83c3bb6214546810805b6cabd",
+    "url": "/static/media/swagger-icon.dd28d3d8.png"
+  }
+]);
